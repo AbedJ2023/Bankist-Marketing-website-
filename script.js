0fb2fc6f43360bde0292402d98ab57b114de2787
Abed <abedj2023@Gmail.com>
2023-09-23 14:04:50 -0500

    navigate through the page from navbar buttons

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,12 +32,8 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener('click', e => {
-  const s1coords = section1.getBoundingClientRect();
-
+  // const s1coords = section1.getBoundingClientRect();
   // Scrolling
   // window.scrollTo(
   //   s1coords.left + window.pageXOffset,
@@ -49,6 +47,29 @@ btnScrollTo.addEventListener('click', e => {
   // });
 
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// page navigation
+// document.querySelectorAll('.nav__link').forEach(el => {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// 1. add event listener to common parent element.
+
+// 2. determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
 });
 
 // /////////////////////////////////////
