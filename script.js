df9215e41167c6ca1f315c1fb35d5e3b521d7afd
Abed <abedj2023@Gmail.com>
2023-09-22 19:49:02 -0500

    scroll to section1 when press learn more

--- script.js
+++ script.js
@@ -30,8 +30,25 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// /////////////////////////////////////
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+btnScrollTo.addEventListener('click', e => {
+  const s1coords = section1.getBoundingClientRect();
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// /////////////////////////////////////
